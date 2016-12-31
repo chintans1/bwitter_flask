4e785b23d34c3df38d8a74f2bcc73829716cdaaa
Chintan Shah <cgintan1998@gmail.com>
2016-12-31 01:53:05 -0700

    PEP8 Styling
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from flask import render_template, flash, redirect
 from app import app
 from .forms import LoginForm
 
-user = {'nickname': 'Chintan'} # fake user
+user = {'nickname': 'Chintan'}  # fake user
 
 
 @app.route('/')
@@ -20,12 +20,13 @@ def index():
     ]
     return render_template('index.html', user=user, bwits=bwits)
 
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
         flash('Login requested for OpenID="%s", remember_me=%s' %
-            (form.openid.data, str(form.remember_me.date)))
+              (form.openid.data, str(form.remember_me.date)))
         return redirect('/index')
     return render_template('login.html', user=user, title="Sign in",
-        form=form, providers=app.config['OPENID_PROVIDERS'])
+                           form=form, providers=app.config['OPENID_PROVIDERS'])
