aa6e1912525f182a750665eae4c4055d1f3e6e18
Chintan Shah <cgintan1998@gmail.com>
2016-12-31 01:47:58 -0700

    Initial commit
    Signed-off-by: Chintan Shah <cgintan1998@gmail.com>

---
+++ app/views.py
@@ -0,0 +1,31 @@
+from flask import render_template, flash, redirect
+from app import app
+from .forms import LoginForm
+
+user = {'nickname': 'Chintan'} # fake user
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    bwits = [   # fake list of bwits
+        {
+            'user': {'nickname': 'John'},
+            'bwit': 'Beautiful day in Portland!'
+        },
+        {
+            'user': {'nickname': 'Susan'},
+            'bwit': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', user=user, bwits=bwits)
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for OpenID="%s", remember_me=%s' %
+            (form.openid.data, str(form.remember_me.date)))
+        return redirect('/index')
+    return render_template('login.html', user=user, title="Sign in",
+        form=form, providers=app.config['OPENID_PROVIDERS'])
