6469947db530fbeda469a03e5117ad1fcd05542b
Chintan Shah <chintans98@gmail.com>
2017-01-23 15:47:16 -0500

    Loggin
    Signed-off-by: Chintan Shah <chintans98@gmail.com>

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,8 @@ from flask_login import LoginManager
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from .momentjs import momentjs
+import sys
+import logging
 
 app = Flask(__name__)
 app.config.from_object('config')
@@ -11,6 +13,9 @@ db = SQLAlchemy(app)
 lm = LoginManager()
 lm.init_app(app)
 lm.login_view = 'login'
+
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.ERROR)
 
 from app import views
 app.jinja_env.globals['momentjs'] = momentjs
