572eb06b72e5270792269218b7e9eac24c3bc96c
Chintan Shah <chintans98@gmail.com>
2017-01-24 00:25:18 -0500

    View list of users
    Signed-off-by: Chintan Shah <chintans98@gmail.com>

--- app/views.py
+++ app/views.py
@@ -1,4 +1,4 @@
-from flask import session, request, flash, url_for, redirect
+from flask import session, flash, url_for, redirect
 from flask import render_template, g
 from flask_login import login_user, logout_user, current_user, login_required
 from app import app, db, lm
@@ -9,6 +9,7 @@ from config import BWITS_PER_PAGE
 from .oauth import OAuthSignIn
 
 
+# Index Page
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/index', methods=['GET', 'POST'])
 @app.route('/index/<int:page>', methods=['GET', 'POST'])
@@ -26,6 +27,13 @@ def index(page=1):
     return render_template('index.html', user=user, bwits=bwits, form=form)
 
 
+# Users List
+@app.route('/users')
+def users_list():
+    users = User.query.all()
+    return render_template('users.html', users=users)
+
+# Login Related Functions
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if g.user is not None and g.user.is_authenticated:
@@ -49,29 +57,6 @@ def before_request():
         g.user.last_seen = datetime.utcnow()
         db.session.add(g.user)
         db.session.commit()
-
-'''
-def after_login(resp):
-    if ((resp.email is None) or (resp.email == '')):
-        flash('Invalid login. Please try again.')
-        return redirect(url_for('login'))
-    user = User.query.filter_by(email=resp.email).first()
-
-    if (user is None):
-        nickname = resp.nickname
-        if ((nickname is None) or (nickname == '')):
-            nickname = resp.email.split('@')[0]
-        user = User(nickname=nickname, email=resp.email)
-        db.session.add(user)
-        db.session.commit()
-    remember_me = False
-
-    if ('remember_me' in session):
-        remember_me = session['remember_me']
-        session.pop('remember_me', None)
-    login_user(user, remember=remember_me)
-    return redirect(request.args.get('next') or url_for('index'))
-'''
 
 
 @app.route('/authorize/<provider>')
@@ -107,6 +92,7 @@ def logout():
     return redirect(url_for('index'))
 
 
+# User Profile
 @app.route('/user/<nickname>')
 @app.route('/user/<nickname>/<int:page>')
 @login_required
@@ -119,6 +105,7 @@ def user(nickname, page=1):
     return render_template('user.html', user=user, bwits=bwits)
 
 
+# Edit User Profile
 @app.route('/edit', methods=['GET', 'POST'])
 @login_required
 def edit():
@@ -136,17 +123,7 @@ def edit():
     return render_template('edit.html', form=form)
 
 
-@app.errorhandler(404)
-def not_found_error(error):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def internal_error(error):
-    db.session.rollback()
-    return render_template('500.html'), 500
-
-
+# User Actions (Follow/Unfollow)
 @app.route('/follow/<nickname>')
 @login_required
 def follow(nickname):
@@ -189,3 +166,14 @@ def unfollow(nickname):
     db.session.commit()
     flash('You have stopped following ' + nickname + '.')
     return redirect(url_for('user', nickname=nickname))
+
+# Errors
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def internal_error(error):
+    db.session.rollback()
+    return render_template('500.html'), 500
